9e55758a672092afa625cd78098105b107722fa4
Izzah Rasdan <izzahrasdan@gmail.com>
2020-09-28 13:03:21 +0800

    Redux Thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux'; //create the Redux store
+import {createStore, combineReducers, applyMiddleware} from 'redux'; //create the Redux store
 import { Dishes } from './dishes'; // needed to configure store
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -11,7 +13,8 @@ export const ConfigureStore = () => {
             comments: Comments,
             promotions: Promotions,
             leaders: Leaders
-        })
+        }),
+        applyMiddleware(thunk, logger)
     );
 
     return store;
--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,10 +1,20 @@
-import { DISHES } from '../shared/dishes';
+import * as ActionTypes from './ActionTypes';
 
 //in implementing Reducer func,
 //it takes state and action as two parameters
-export const Dishes = (state = DISHES, action) => {
+export const Dishes = (state = {
+    isLoading: true,
+    errMess: null,
+    dishes:[]
+    }, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_DISHES:
+            return {...state, isLoading: true, errMess: null, dishes: action.payload};
+        case ActionTypes.DISHES_LOADING:
+            return {...state, isLoading: true, errMess: null, dishes: []};
+        case ActionTypes.DISHES_FAILED:
+            return {...state, isLoading: true, errMess: action.payload, dishes: []};
         default:
             return state;
     }
-}
+};
